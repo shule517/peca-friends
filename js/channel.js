3c55794d952ddcfc62e9c6ec08753239617b1e82
shule517 <shule517@gmail.com>
2017-03-20 19:33:17 +0900

    未配信のお気に入り配信を表示

--- js/channel.js
+++ js/channel.js
@@ -4,10 +4,41 @@ let app = angular.module('App', []);
 app.controller('mainCtrl', function($scope, $http){
     let jsonFilePath = "favorite.json"
 
+    getHistory = (channelName) => {
+        url = 'https://peca-tsu.herokuapp.com/channels?name=' + channelName
+        $http.get(url, {})
+            .success((data, status, headers, config) => {
+                console.log('success');
+                if (data.length > 0) {
+                    let history = {
+                        name: data[0].name,
+                        genre: data[0].last_genre,
+                        details: data[0].last_detail,
+                        contactUrl: data[0].contact_url,
+                        lastStartedAt: data[0].last_started_at,
+                        lastComment: data[0].last_comment,
+                        icon: './img/not-live.png'
+                    }
+                    if (!$scope.favoriteChannels.map((ch) => { return ch.name; }).includes(channelName)) {
+                        // いなければ追加
+                        $scope.favoriteChannels.push(history)
+                    }
+                }
+            })
+            .error((data, status, headers, config) => {
+                console.log('error');
+            });
+    }
+
     // お気に入り
     $scope.favoriteChannelNames = [];
     $scope.favoriteChannels = [];
     try { $scope.favoriteChannelNames = JSON.parse(fs.readFileSync(jsonFilePath, 'utf8')); } catch (err){ console.log(err) }
+
+    $scope.favoriteChannelNames.forEach((channelName) => {
+        getHistory(channelName)
+    });
+
     $scope.favorite = (channel) => {
         console.log("お気に入り:" + channel.name)
         if ($scope.isFavorite(channel)) {
@@ -54,6 +85,10 @@ app.controller('mainCtrl', function($scope, $http){
                         $scope.channels.push(channel);
                     }
                     if ($scope.favoriteChannelNames.includes(channel.name)) {
+                        if ($scope.favoriteChannels.includes(channel)) {
+                            let index = $scope.favoriteChannels.indexOf(channel);
+                            $scope.favoriteChannels.splice(index, 1);
+                        }
                         $scope.favoriteChannels.push(channel);
                     }
                 });
